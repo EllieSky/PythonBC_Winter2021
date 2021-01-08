f60ccc21751f4d05609e0922dca79e78f966af39
Ellie Yampolskaya <blackellie@yahoo.com>
2021-01-07 23:02:00 -0800

    Session 5

---
+++ beach_body.py
@@ -0,0 +1,25 @@
+import time
+import unittest
+
+from selenium import webdriver
+
+
+class BeachBody(unittest.TestCase):
+    def test_get_started_button(self):
+        browser = webdriver.Chrome(executable_path='/Users/ellie/Automation/PythonBC_Winter2021/chromedriver')
+        browser.get('https://www.beachbody.com/')
+
+        browser.find_element_by_class_name('button').click()
+
+        time.sleep(1)
+
+        expected_url = 'https://www.beachbody.com/create-account?productCode=BODGEN&rc=BODGEN'
+        actual_url = browser.current_url
+
+        self.assertEqual(expected_url, actual_url)
+
+        self.assertIn("create-account", actual_url)
+
+
+if __name__ == '__main__':
+    unittest.main()
