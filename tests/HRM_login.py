72bd1191c258e305cd85fecebde7fabc11834881
Hanna Mazur <hanna_mazur@hotmail.com>
2021-01-12 23:18:53 -0800

    homework 6: negative testing for login

--- tests/HRM_login.py
+++ tests/HRM_login.py
@@ -5,11 +5,22 @@ import time
 
 from selenium import webdriver
 
+from tests import CHROME_PATH
+
 
 class BasicLogin(unittest.TestCase):
-    def test_valid_login(self):
+    def setUp(self) -> None:
         #browser session
-        browser = webdriver.Chrome(executable_path='/Users/hannamazur/Projects/PythonBC_Winter2021/chromedriver')
+        browser = webdriver.Chrome(executable_path=CHROME_PATH)
+        # Take browser out of local browser variable into SELF
+        self.browser = browser
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_valid_login(self):
+        # Take browser out of SELF into local browser variable
+        browser = self.browser
         #open the URL
         browser.get('http://hrm-online.portnov.com/')
         #enter username
@@ -24,6 +35,63 @@ class BasicLogin(unittest.TestCase):
         welcome_message = browser.find_element_by_id('welcome').text
         self.assertEqual('Welcome Admin', welcome_message)
         self.assertTrue(browser.find_element_by_id('welcome').is_displayed())
+
+
+    def test_empty_user_name_valid_password(self):
+        browser = self.browser
+        browser.get('http://hrm-online.portnov.com/')
+        browser.find_element_by_id('txtUsername').send_keys('')
+        browser.find_element_by_id('txtPassword').send_keys('password')
+        browser.find_element_by_id('btnLogin').click()
+        time.sleep(1)
+        self.assertTrue(browser.current_url.endswith('/auth/login'))
+        error_message = browser.find_element_by_id('spanMessage').text
+        self.assertEqual('Username cannot be empty', error_message)
+
+    def test_empty_password_valid_username(self):
+        browser = self.browser
+        browser.get('http://hrm-online.portnov.com/')
+        browser.find_element_by_id('txtUsername').send_keys('admin')
+        browser.find_element_by_id('txtPassword').send_keys('')
+        browser.find_element_by_id('btnLogin').click()
+        time.sleep(1)
+        self.assertTrue(browser.current_url.endswith('/auth/login'))
+        error_message = browser.find_element_by_id('spanMessage').text
+        self.assertEqual('Password cannot be empty', error_message)
+
+    def test_empty_password_empty_username(self):
+        browser = self.browser
+        browser.get('http://hrm-online.portnov.com/')
+        browser.find_element_by_id('txtUsername').send_keys('')
+        browser.find_element_by_id('txtPassword').send_keys('')
+        browser.find_element_by_id('btnLogin').click()
+        time.sleep(1)
+        self.assertTrue(browser.current_url.endswith('/auth/login'))
+        error_message = browser.find_element_by_id('spanMessage').text
+        self.assertEqual('Username cannot be empty', error_message)
+
+    def test_valid_username_invalid_password(self):
+        browser = self.browser
+        browser.get('http://hrm-online.portnov.com/')
+        browser.find_element_by_id('txtUsername').send_keys('admin')
+        browser.find_element_by_id('txtPassword').send_keys('qwerty')
+        browser.find_element_by_id('btnLogin').click()
+        time.sleep(1)
+        self.assertTrue(browser.current_url.endswith('/auth/validateCredentials'))
+        error_message = browser.find_element_by_id('spanMessage').text
+        self.assertEqual('Invalid credentials', error_message)
+        self.assertTrue(browser.find_element_by_id('spanMessage').is_displayed())
+
+    def test_invalid_user_name_invalid_password(self):
+        browser = self.browser
+        browser.get('http://hrm-online.portnov.com/')
+        browser.find_element_by_id('txtUsername').send_keys('test')
+        browser.find_element_by_id('txtPassword').send_keys('test')
+        browser.find_element_by_id('btnLogin').click()
+        time.sleep(1)
+        self.assertTrue(browser.current_url.endswith('/auth/validateCredentials'))
+        error_message = browser.find_element_by_id('spanMessage').text
+        self.assertEqual('Invalid credentials', error_message)
 
 
 if __name__ == '__main__':
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,11 @@
 import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJ_PATH = os.path.dirname(TESTS_DIR)
-CHROME_PATH = os.path.join(PROJ_PATH,'drivers','chromedriver')
+
+#It will check ChromeDriver version and browser version. It'll install new version if needed.Slower
+CHROME_PATH = ChromeDriverManager().install()
+
+#if you whant faster use below, but you need to check versions manually
+#CHROME_PATH = os.path.join(PROJ_PATH,'drivers','chromedriver')
