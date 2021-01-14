75e62abf555226d4f01241c50749baa41482fbcb
frolovde <denisfrolov@gmail.com>
2021-01-14 13:32:19 -0800

    Created six negative tests for OrangeHRM

--- tests/Orange_HRM_Denis.py
+++ tests/Orange_HRM_Denis.py
@@ -34,7 +34,76 @@ class OrangeHRMLogin(unittest.TestCase):
         self.assertEqual("Welcome Admin", welcome_admin)
 
 
+    def test_negative_scenario_one(self):
+        driver = self.driver
+        driver.get("http://hrm-online.portnov.com/")
+        time.sleep(1)
+        driver.find_element_by_name("Submit").click()
+        error_one = driver.find_element_by_id("spanMessage")
+        assert error_one.is_displayed()
+        error_one = driver.find_element_by_id("spanMessage").text
+        self.assertEqual("Username cannot be empty", error_one)
 
+    def test_negative_scenario_two(self):
+        driver = self.driver
+        driver.get("http://hrm-online.portnov.com/")
+        time.sleep(1)
+        driver.find_element_by_id("txtUsername").send_keys("admin")
+        driver.find_element_by_name("Submit").click()
+        error_two = driver.find_element_by_id("spanMessage")
+        assert error_two.is_displayed()
+        error_two = driver.find_element_by_id("spanMessage").text
+        self.assertEqual("Password cannot be empty", error_two)
+
+    def test_negative_scenario_three(self):
+        driver = self.driver
+        driver.get("http://hrm-online.portnov.com/")
+        time.sleep(1)
+        driver.find_element_by_id("txtPassword").send_keys("password")
+        driver.find_element_by_name("Submit").click()
+        error_three = driver.find_element_by_id("spanMessage")
+        assert error_three.is_displayed()
+        error_three = driver.find_element_by_id("spanMessage").text
+        self.assertEqual("Username cannot be empty", error_three)
+
+    def test_negative_scenario_four(self):
+        driver = self.driver
+        driver.get("http://hrm-online.portnov.com/")
+        time.sleep(1)
+        driver.find_element_by_id("txtUsername").send_keys("admin1")
+        driver.find_element_by_id("txtPassword").send_keys("password")
+        driver.find_element_by_name("Submit").click()
+        error_four = driver.find_element_by_id("spanMessage")
+        assert error_four.is_displayed()
+        error_four = driver.find_element_by_id("spanMessage").text
+        self.assertEqual("Invalid credentials", error_four)
+        self.assertTrue(driver.current_url.endswith("validateCredentials"))
+
+    def test_negative_scenario_five(self):
+        driver = self.driver
+        driver.get("http://hrm-online.portnov.com/")
+        time.sleep(1)
+        driver.find_element_by_id("txtUsername").send_keys("ADMIN")
+        driver.find_element_by_id("txtPassword").send_keys("PASSWORD")
+        driver.find_element_by_name("Submit").click()
+        error_five = driver.find_element_by_id("spanMessage")
+        assert error_five.is_displayed()
+        error_four = driver.find_element_by_id("spanMessage").text
+        self.assertEqual("Invalid credentials", error_four)
+        self.assertTrue(driver.current_url.endswith("validateCredentials"))
+
+    def test_negative_scenario_six(self):
+        driver = self.driver
+        driver.get("http://hrm-online.portnov.com/")
+        time.sleep(1)
+        driver.find_element_by_id("txtUsername").send_keys("&^*%$#%")
+        driver.find_element_by_id("txtPassword").send_keys("password")
+        driver.find_element_by_name("Submit").click()
+        error_six = driver.find_element_by_id("spanMessage")
+        assert error_six.is_displayed()
+        error_four = driver.find_element_by_id("spanMessage").text
+        self.assertEqual("Invalid credentials", error_four)
+        self.assertTrue(driver.current_url.endswith("validateCredentials"))
 
 if __name__ == '__main__':
     unittest.main()
