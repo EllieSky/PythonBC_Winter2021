6164aa67e50344b68cc66dd5bfd543cde00cc300
bpechersky <bpechersky@workmail.com>
2021-01-15 11:18:39 -0800

    added functions for search by employee id and using esc key

--- tests/emp_search.py
+++ tests/emp_search.py
@@ -2,6 +2,7 @@ import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
 from tests import CHROME_PATH
@@ -45,7 +46,21 @@ class EmpSearch(unittest.TestCase):
 
         for item in list_of_web_elements_jtitles:
             self.assertEqual('SDET', item.text)
-
+    def test_search_by_employee_id(self):
+        self.login('admin', 'password')
+        self.browser.find_element_by_id('empsearch_id').send_keys('635919')
+        self.browser.find_element_by_id('searchBtn').click()
+        actual_search_result = self.browser.find_element_by_link_text('635919').text
+        print('result' +  actual_search_result)
+        self.assertTrue('635919' == actual_search_result)
+        list_of_web_elements_jtitles = self.browser.find_elements_by_xpath('//*[@id="resultTable"]/tbody/tr/td[5]')
+      #  print("lenght of list is " + str(len(list_of_web_elements_jtitles)))
+        self.assertTrue(len(list_of_web_elements_jtitles)==1)
+    def test_send_esc_key(self):
+        self.login('admin','password')
+        self.browser.find_element_by_id('empsearch_employee_name_empName').send_keys('David')
+        self.browser.find_element_by_id('empsearch_employee_name_empName').send_keys(Keys.ESCAPE)
+        time.sleep(4)
 
 
 if __name__ == '__main__':
