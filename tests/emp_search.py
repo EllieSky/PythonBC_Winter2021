ceea5a3e369a7aa3bb76c440624aeba3aadbf75f
Ellie Yampolskaya <blackellie@yahoo.com>
2021-01-19 21:41:23 -0800

    Session 8: first half of class

--- tests/emp_search.py
+++ tests/emp_search.py
@@ -2,6 +2,7 @@ import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
 from tests import CHROME_PATH
@@ -45,6 +46,54 @@ class EmpSearch(unittest.TestCase):
 
         for item in list_of_web_elements_jtitles:
             self.assertEqual('SDET', item.text)
+
+    def test_search_by_id(self):
+        emp_id = '3250'
+
+        self.login('admin', 'password')
+        self.browser.find_element_by_id('empsearch_id').send_keys(emp_id)
+
+        self.browser.find_element_by_id('searchBtn').click()
+
+        time.sleep(2)
+
+        list_of_rows = self.browser.find_elements_by_xpath('//*[@id="resultTable"]/tbody/tr')
+        # self.assertTrue(len(list_of_rows) == 1)
+        # OR
+        self.assertEqual(1, len(list_of_rows))
+
+
+        result = self.browser.find_element_by_xpath('//*[@id="resultTable"]/tbody/tr/td[2]').text
+        self.assertEqual(emp_id, result)
+
+    def test_search_by_emp_name(self):
+        name = "amb"
+
+        self.login('admin', 'password')
+        self.browser.find_element_by_id('empsearch_employee_name_empName').send_keys(name)
+        self.browser.find_element_by_id('empsearch_employee_name_empName').send_keys(Keys.ESCAPE)
+
+        self.assertFalse(self.browser.find_element_by_class_name('ac_results').is_displayed())
+
+        self.browser.find_element_by_id('searchBtn').click()
+
+        time.sleep(2)
+
+        list_of_rows = self.browser.find_elements_by_xpath('//*[@id="resultTable"]/tbody/tr')
+
+        for single_row in list_of_rows:
+            first_name = single_row.find_elements_by_xpath(".//td[3]").text.lower()
+            last_name = single_row.find_elements_by_xpath(".//td[4]").text.lower()
+
+            self.assertIn(name, [first_name, last_name])
+            # OR
+            self.assertTrue(first_name.find(name) != -1 or last_name.find(name) != -1)
+
+        for index in range(len(list_of_rows)):
+            first_name = self.browser.find_element_by_xpath('//*[@id="resultTable"]/tbody/tr[' + (index + 1) + ']/td[3]').text
+            last_name = self.browser.find_element_by_xpath('//*[@id="resultTable"]/tbody/tr[' + (index + 1) + ']/td[4]').text
+
+            self.assertTrue(name in first_name + last_name)
 
 
 
