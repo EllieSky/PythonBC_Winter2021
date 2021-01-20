11468101e664240ce14b22032328906c6df996ff
frolovde <denisfrolov@gmail.com>
2021-01-20 14:33:46 -0800

    Session 8 homework. Created Variables and made changes to the Wait. Please review again, thanks!

--- tests/Orange_HRM_Add_Employee.py
+++ tests/Orange_HRM_Add_Employee.py
@@ -31,10 +31,18 @@ class OrangeHRMEmpAdd(unittest.TestCase):
         self.driver.find_element_by_id('btnLogin').click()
 
     def test_emp_add_user(self):
+        self.login('admin', 'password')
+
+        # Test Variables
         driver = self.driver
         desired_url = 'http://hrm-online.portnov.com/symfony/web/index.php/pim/addEmployee'
         wait = WebDriverWait(driver, 15)
-        self.login('admin', 'password')
+        first_name = "Denis"
+        last_name = "Frolov"
+        user_name = "Denisfrolov"
+        new_password = "password"
+        picture = "C:/Users/Denis/Dropbox/My PC (DESKTOP-KJ79GMA)/Documents/Silver Auto Python Bootcamp/upload files/test.jpg"
+        expected_login_url = "http://hrm-online.portnov.com/symfony/web/index.php/auth/login"
 
         # Wait provided by Ellie
 
@@ -47,40 +55,40 @@ class OrangeHRMEmpAdd(unittest.TestCase):
 
         wait.until(EC.url_to_be(desired_url))
         wait.until(EC.presence_of_element_located([By.ID, "firstName"]))
-        driver.find_element_by_id("firstName").send_keys("Denis")
-        driver.find_element_by_id("lastName").send_keys("Frolov")
+        driver.find_element_by_id("firstName").send_keys(first_name)
+        driver.find_element_by_id("lastName").send_keys(last_name)
         employee_id = driver.find_element_by_id("employeeId").get_attribute("value")
 
         # Uploading a test profile picture
 
         upload_file = driver.find_element_by_id("photofile")
-        upload_file.send_keys("C:/Users/Denis/Dropbox/My PC (DESKTOP-KJ79GMA)/Documents/Silver Auto Python Bootcamp/upload files/test.jpg")
+        upload_file.send_keys(picture)
         driver.find_element_by_id("chkLogin").click()
 
         # Waiting for the fields to become visible
 
         wait.until(EC.visibility_of_element_located([By.ID, "user_name"]))
-        driver.find_element_by_id("user_name").send_keys("Denisfrolov" + employee_id)
-        driver.find_element_by_id("user_password").send_keys("password")
-        driver.find_element_by_id("re_password").send_keys("password")
+        driver.find_element_by_id("user_name").send_keys(user_name + employee_id)
+        driver.find_element_by_id("user_password").send_keys(new_password)
+        driver.find_element_by_id("re_password").send_keys(new_password)
         driver.find_element_by_id("status").click()
         driver.find_element_by_id("btnSave").click()
 
-        # 2 Waits for practice
 
-        wait.until(EC.url_changes)
-        wait.until((EC.presence_of_element_located([By.ID, "btnSave"])))
+        # Making sure Personal Details is displayed
+
+        wait.until((EC.presence_of_element_located([By.XPATH, "//h1[contains(text(),'Personal Details')]"])))
         driver.find_element_by_id("welcome").click()
 
         # This is a wait for the logout overlay to be visible
 
         wait.until(EC.visibility_of_element_located([By.XPATH, "//a[contains(text(),'Logout')]"]))
         driver.find_element_by_xpath("//a[contains(text(),'Logout')]").click()
-        self.assertTrue(driver.current_url, "http://hrm-online.portnov.com/symfony/web/index.php/auth/login")
+        self.assertTrue(driver.current_url, expected_login_url)
 
 # Logging in with a newly created user
 
-        self.login('Denisfrolov' + employee_id, 'password')
+        self.login(user_name + employee_id, new_password)
         wait.until(EC.presence_of_element_located([By.ID, "welcome"]))
         welcome = driver.find_element_by_id("welcome").text
         self.assertEqual("Welcome Denis", welcome)
