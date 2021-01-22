8adee278a7ecc20b5689a5b8c8ac2cf511181c4d
frolovde <denisfrolov@gmail.com>
2021-01-21 22:00:07 -0800

    Session 9 classwork. Updated code

---
+++ fixtures/fixture.py
@@ -0,0 +1,18 @@
+import unittest
+
+from selenium import webdriver
+from pages.login import LoginPage
+from tests import CHROME_PATH
+
+
+class BaseFixture(unittest.TestCase):
+    def setUp(self) -> None:
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        self.driver = driver
+        driver.get("http://hrm-online.portnov.com/")
+        self.login_page = LoginPage(driver)
+        self.login_page.login()
+
+    def tearDown(self) -> None:
+        self.driver.quit()
