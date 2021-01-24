51d9c2006d918799f382e1e032bd1dcf69ece910
NikitaRogon <77036006+NikitaRogon@users.noreply.github.com>
2021-01-24 18:29:59 -0500

    class work and homework6

--- tests/6th_class_practice.py
+++ tests/6th_class_practice.py
@@ -30,7 +30,23 @@ class BasicLogIn(unittest.TestCase):
         welcome_message=browser.find_element_by_id("welcome").text
 
         self.assertEqual("Welcome Admin", welcome_message)
+    def test_invalid_login(self):
+        browser=self.browser
 
+        browser.get('http://hrm-online.portnov.com')
+        browser.find_element_by_id("txtUsername").send_keys("dfwe")
+        browser.find_element_by_id("txtPassword").send_keys("nkjjn")
+        browser.find_element_by_id("btnLogin").click()
+        time.sleep(1)
+        self.assertTrue(browser.current_url.endswith('validateCredentials'))
+        spanMessage = browser.find_element_by_id('spanMessage').text
+
+        self.assertEqual("Invalid credentials", spanMessage)
+
+    # def test_empty_login(self):
+    #     browser = self.browser
+    #     self.browser.get('http://hrm-online.portnov.com')
+    #
 
 if __name__ == '__main__':
     unittest.main()
