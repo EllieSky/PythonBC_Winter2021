e160471672132e026e1d5d405c0db5399c34a5e3
NikitaRogon <77036006+NikitaRogon@users.noreply.github.com>
2021-01-24 15:51:22 -0500

    class work and homework6

--- tests/6th_class_practice.py
+++ tests/6th_class_practice.py
@@ -3,11 +3,19 @@ import unittest
 
 from selenium import webdriver
 
+from tests import CHROME_PATH
+
 
 class BasicLogIn(unittest.TestCase):
-    def test_valid_login(self):
-        browser = webdriver.Chrome(executable_path="/drivers/chromedriver.exe")
+    def setUp(self) -> None:
+        browser = webdriver.Chrome(executable_path=CHROME_PATH)
+        self.browser = browser
 
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_valid_login(self):
+        browser=self.browser
 
         browser.get('http://hrm-online.portnov.com')
 
@@ -16,7 +24,7 @@ class BasicLogIn(unittest.TestCase):
         browser.find_element_by_id("txtPassword").send_keys("password")
 
         browser.find_element_by_id("btnLogin").click()
-        time.sleep(1)
+        time.sleep(3)
 
         self.assertTrue(browser.current_url.endswith("pim/viewEmployeeList"))
         welcome_message=browser.find_element_by_id("welcome").text
