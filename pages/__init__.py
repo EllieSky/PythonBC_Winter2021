e08a28f517d40574d5ee8362f5dedda712018f17
NikitaRogon <77036006+NikitaRogon@users.noreply.github.com>
2021-01-24 15:52:54 -0500

    __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,7 +1,10 @@
 
 import os
 
-TEST_DIR= os.path.dirname(os.path.abspath(__file__))
-PROJ_PATH = os.path.dirname(TEST_DIR)
-CHROME_PATH = os.path.join(PROJ_PATH, 'drivers', 'chromedriver.exe')
+from webdriver_manager.chrome import ChromeDriverManager
 
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJ_PATH = os.path.dirname(TEST_DIR)
+# CHROME_PATH = os.path.join(PROJ_PATH, 'drivers', 'chromedriver.exe')
+
+CHROME_PATH = ChromeDriverManager().install()
