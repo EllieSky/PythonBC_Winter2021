cc1090b3dbe11ec41443b6bb7a3446abf130ddf8
NikitaRogon <77036006+NikitaRogon@users.noreply.github.com>
2021-01-25 21:53:02 -0500

    tests/6th_class_practice.py

--- tests/6th_class_practice.py
+++ tests/6th_class_practice.py
@@ -10,50 +10,45 @@ class BasicLogIn(unittest.TestCase):
     def setUp(self) -> None:
         browser = webdriver.Chrome(executable_path=CHROME_PATH)
         self.browser = browser
+        browser.get('http://hrm-online.portnov.com/')
 
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def login(self, username, password):
+        if username:
+            self.browser.find_element_by_id("txtUsername").send_keys(username) if username else  None
+        if password:
+            self.browser.find_element_by_id("txtPassword").send_keys(password)
+
+        self.browser.find_element_by_id("btnLogin").click()
+
+
     def test_valid_login(self):
         browser=self.browser
-
-        browser.get('http://hrm-online.portnov.com')
-
-        browser.find_element_by_id("txtUsername").send_keys("admin")
-
-        browser.find_element_by_id("txtPassword").send_keys("password")
-
-        browser.find_element_by_id("btnLogin").click()
+        self.login("admin", "password")
         time.sleep(3)
 
         self.assertTrue(browser.current_url.endswith("pim/viewEmployeeList"))
         welcome_message=browser.find_element_by_id("welcome").text
-
         self.assertEqual("Welcome Admin", welcome_message)
+
     def test_invalid_login(self):
-        browser=self.browser
-
-        browser.get('http://hrm-online.portnov.com')
-        browser.find_element_by_id("txtUsername").send_keys("dfwe")
-        browser.find_element_by_id("txtPassword").send_keys("nkjjn")
-        browser.find_element_by_id("btnLogin").click()
+        browser = self.browser
+        self.login("jbkjh", "knlkn")
         time.sleep(1)
-        self.assertTrue(browser.current_url.endswith('validateCredentials'))
-        spanMessage = browser.find_element_by_id('spanMessage').text
 
+        spanMessage = browser.find_element_by_id('spanMessage').text
         self.assertEqual("Invalid credentials", spanMessage)
 
-    def test_empty_username_login(self):
+    def test_empty_password_login(self):
         browser = self.browser
-        self.browser.get('http://hrm-online.portnov.com')
+        self.login("admin", None)
 
-        browser.find_element_by_id("txtPassword").send_keys("Password")
-        browser.find_element_by_id("btnLogin").click()
         time.sleep(3)
 
         failing_massege = browser.find_element_by_id('spanMessage').text
-
-        self.assertEqual("Username cannot be empty", failing_massege)
+        self.assertEqual("Password cannot be empty", failing_massege)
 
 
 if __name__ == '__main__':
