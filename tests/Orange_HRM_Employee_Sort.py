e023a6c264bd5e768625e362688232fd77a7949e
frolovde <denisfrolov@gmail.com>
2021-01-25 22:17:55 -0800

    Finished first half of HW

--- tests/Orange_HRM_Employee_Sort.py
+++ tests/Orange_HRM_Employee_Sort.py
@@ -18,27 +18,31 @@ class OrangeHRMEmpEmployeeSort(BaseFixture):
         wait = WebDriverWait(driver, 15)
 
 
-        wait.until(EC.presence_of_element_located([By.CSS_SELECTOR, '#empsearch_employee_name_empName.inputFormatHint']))
+        wait.until(
+            EC.presence_of_element_located([By.CSS_SELECTOR, '#empsearch_employee_name_empName.inputFormatHint']))
+
         driver.find_element_by_link_text("First (& Middle) Name").click()
     # Waiting for sort to complete by checking URL change
         wait.until(EC.url_contains, "firstMiddleName&sortOrder=ASC")
-        page_info = driver.find_element_by_xpath("//li[@class='desc']").text
-        print(page_info)
 
+    # Introducing Variables
         names_table = driver.find_elements_by_xpath("//tbody/tr/td[3]")
         list_of_names = []
+        next_button = driver.find_element_by_xpath("//*/div[1]/ul/li[6]/a")
 
     # Getting a list of First names from the page, adding them to the list and comparing with the same list but sorted.
+
         for i in range(len(names_table)):
             name = names_table[i].text.lower()
             list_of_names.append(name)
-            print(list_of_names)
             self.assertEqual(list_of_names, sorted(list_of_names))
-
-
-
-
-
+    # Clicking next button if there is more than one page
+        if next_button.is_displayed():
+            next_button.click()
+            wait.until(EC.url_contains, "viewEmployeeList")
+            time.sleep(2)
+        else:
+            print("There is only one page")
 
 
 if __name__ == '__main__':
